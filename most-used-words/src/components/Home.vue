7674c3e4994776920a2c12f46d6078a7d51f9cd4
Marcio Oliveira <marciotojr@gmail.com>
2020-06-04 23:47:07 -0300

    feat(front): :sparkles: Criação da base do front end

---
+++ most-used-words/src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container fluid>
+      <v-form>
+          <v-file-input 
+            v-model="files"
+            multiple
+            chips 
+            counter
+            label="Select your subtitles"
+            placeholder="Select your subtitles"
+            prepend-icon="mdi-paperclip"
+            outlined
+            :show-size="1000"
+            append-outer-icon="mdi-send"
+            @click:append-outer="processSubtitles">
+
+          </v-file-input>
+      </v-form>
+      <div class='pills'>
+          <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount" />
+      </div>
+  </v-container>
+</template>
+
+<script>
+import Pill from './Pill'
+
+export default {
+    components: { Pill },
+    data: function() {
+        return {
+            files:[],
+            groupedWords: [
+                {name: 'I', amount: 1234},
+                {name: 'you', amount: 999},
+                {name: 'he', amount: 666},
+            ]
+        }
+    },
+    methods:{
+        processSubtitles(){
+            console.log(this.files)
+        }
+    }
+}
+</script>
+
+<style>
+    .pills{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
+</style>
---
+++ most-used-words/src/components/Pill.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="pill">
+        <span class="name">{{ name }}</span>
+        <span class="amount">{{ amount }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['name', 'amount']
+}
+</script>
+
+<style scoped>
+
+    @keyframes shadow-fade {
+        to {box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);}
+    }
+
+    .pill {
+        background-color: teal;
+        color: white;
+        
+        margin: 10px;
+        height: 55px;
+        border-radius:30px;
+        
+        font-size: 1.3rem;
+        font-weight: bold;
+
+        display: flex;
+        align-items: center;
+        
+    }
+
+    .pill:hover{
+        animation-name: shadow-fade;
+        animation-duration: 1s;
+        animation-timing-function: ease-in-out;
+        animation-fill-mode: both;
+        animation-direction: alternate;
+    }
+
+    .pill .name{
+        margin-left: 15px;
+    }
+
+    .pill .amount{
+        margin: 10px;
+        padding: 5px;
+        height: 35px;
+        width: 35px;
+        border-radius: 18px;
+
+        background-color: tomato;
+        font-size: 14px;
+        
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
